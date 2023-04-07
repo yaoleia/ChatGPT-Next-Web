9bd3139b826a9342d8e955fa97d83050083aa59e
姚磊 <yaolei@ainnovation.com>
2023-04-07 12:23:01 +0800

    test: dynamic; revalidate;

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import { Analytics } from "@vercel/analytics/react";
 
 import { Home } from "./components/home";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function App() {
   return (
     <>
